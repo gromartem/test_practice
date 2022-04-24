c4b80277d1d988e7b5d0d1838b9d4c606e418e5f
Artem <moriartythesame@gmail.com>
2022-04-24 12:35:09 +0200

    oject method

--- script.js
+++ script.js
@@ -33,108 +33,109 @@
 // //Циклы
 // //While
 // let num = 50;
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-// //do
-// let num = 50;
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+// // //do
+// // let num = 50;
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num < 55);
 
-// //for 
+// // //for 
 
-// let num = 50;
+// // let num = 50;
 
-// for (let i = 1; i <= 8; i++){
-//     console.log(num);
-//     num++;
+// // for (let i = 1; i <= 8; i++){
+// //     console.log(num);
+// //     num++;
     
-// }
+// // }
 
-// for (let i =1;i < 10;i++){
-//     if (i === 6){
-//         break;//оператор позволяет прервать цикл до 6.
-//     }
-//     console.log(i);
-// }
+// // for (let i =1;i < 10;i++){
+// //     if (i === 6){
+// //         break;//оператор позволяет прервать цикл до 6.
+// //     }
+// //     console.log(i);
+// // }
 
-// for (let i =1;i < 10;i++){
-//     if (i === 6){
-//         continue;//оператор позволяет пропустить шаг но не прерывает цикл.
-//     }
-//     console.log(i);
-// }
-// практика 2 
- " use strict";
+// // for (let i =1;i < 10;i++){
+// //     if (i === 6){
+// //         continue;//оператор позволяет пропустить шаг но не прерывает цикл.
+// //     }
+// //     console.log(i);
+// // }
+// // практика 2 
+//  " use strict";
 
  
-let numberOfFilms;
+// let numberOfFilms;
 
- function start(){
-     numberOfFilms = +prompt ('Сколько фильмов уже гялнул?');
+//  function start(){
+//      numberOfFilms = +prompt ('Сколько фильмов уже глянул?');
     
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-         numberOfFilms = +prompt ('Сколько фильмов уже гялнул?');
-    }
- }
-start();
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//          numberOfFilms = +prompt ('Сколько фильмов уже глянул?');
+//     }
+//  }
+// start();
 
- const personalMovieDB = {
-     count: numberOfFilms,
-     movies: {},
-     actors:{},
-     genres:[],
-     private: false
- };
+//  const personalMovieDB = {
+//      count: numberOfFilms,
+//      movies: {},
+//      actors:{},
+//      genres:[],
+//      private: false
+//  };
 
-function rememberMyFilms(){
-for(let i = 0; i < 2; i++) {
+// function rememberMyFilms(){
+// for(let i = 0; i < 2; i++) {
 
-    const a = prompt('Один из последних фильмов'),
-          b = prompt('Оценка');
+//     const a = prompt('Один из последних фильмов'),
+//           b = prompt('Оценка');
 
-    if (a != null && b != null && a != '' && b != '' && a.length  < 50){
-        personalMovieDB.movies[a] = b;
-    } else {
-        console.log("Попробуй ещё раз");
-        i--;
-    }
-}
-}
+//     if (a != null && b != null && a != '' && b != '' && a.length  < 50){
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log("Попробуй ещё раз");
+//         i--;
+//     }
+// }
+// }
+// rememberMyFilms();
 
-rememberMyFilms();
-function detectedPersonalLevel(){
-if (personalMovieDB.count < 10){
-    console.log("мало фильмов");
-} else if  (personalMovieDB.count >= 10 && personalMovieDB.count  < 30){
-    console.log("Классика ");
-} else if  (personalMovieDB.count >= 30 ){
-    console.log("Любите фильмы! ");
-}else {
-    console.log("Печально, это ошибка ");
-}
-}
+// function detectedPersonalLevel(){
+// if (personalMovieDB.count < 10){
+//     console.log("мало фильмов");
+// } else if  (personalMovieDB.count >= 10 && personalMovieDB.count  < 30){
+//     console.log("Классика ");
+// } else if  (personalMovieDB.count >= 30 ){
+//     console.log("Любите фильмы! ");
+// }else {
+//     console.log("Печально, это ошибка ");
+// }
+// }
 
-detectedPersonalLevel();
+// detectedPersonalLevel();
 
-function showMyDB (hidden){
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.private);
+// function showMyDB (hidden){
+//     if (!hidden){
+//         console.log(personalMovieDB);
+//     }
+// }
+// showMyDB(personalMovieDB.private);
 
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i -1] = genre;
-    }
-}
-writeYourGenres();
+// function writeYourGenres(){
+//     for(let i = 1; i <= 3; i++){
+//         //const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i -1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//     }
+// }
+// writeYourGenres();
+
 // //Функции 
 // //Function Declaration где угодно можно обьявить
 //  function showFirstMessage(text){
@@ -198,3 +199,60 @@ writeYourGenres();
 //  const nem ="12.9px";d
 //  console.log(parseFloat(nem));
 
+//callback Функции
+
+function first(){
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second(){
+    console.log(2);
+}
+first();
+second();
+
+function learnJs(lang, callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done(){
+    console.log('Я прошел жтот урок!');
+
+}
+
+learnJs('JavaScript', done);
+
+learnJs('JavaScript', function(){
+    console.log("Я прошел этот урок");
+});
+
+//обьекты, деструктуризация обьектов 
+const options = {
+    name: ' test',
+    width: 1024,
+    height: 1024,
+    px: 1024,
+    colors:{
+        border:'black',
+        bg: 'red'
+    }
+};
+// console.log(options["colors"]["border"]);
+//.log(options.name);
+
+// delete options.name; //удаляем свойство из обьекта
+//console.log(options);
+
+for ( let key in options) { // перебрать обьект внутри обьекта
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]){
+            console.log(`Свойства ${i} имеет значение ${options[key][i]} `);
+    }
+    }else{
+        console.log(`Свойства ${key} имеет значение ${options[key]} `);//Перебрать свойства и значение
+    }
+    
+}
